775aa368df64d21399daa8ad80513d50ddcb1720
andreasbundgaard <andreasb94@live.dk>
2015-08-31 10:24:05 +0200

    Controller class

---
+++ WpfApplication1/WpfApplication1/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Controller
+    {
+        
+
+
+        public List<Person> CreatePersons()
+        {
+            List<Person> personlist = new List<Person>();
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            personlist.Add(person1);
+            personlist.Add(person2);
+            
+            person1.Username = "Ivan123";
+            person1.Password = "123ivan";
+            person1.Gender = "Male";
+            person1.Weight = 110;
+
+            person2.Username = "Ida123";
+            person2.Password = "123ida";
+            person2.Gender = "Female";
+            person2.Weight = 50;
+
+            return personlist;
+        }
+               
+    }
+}
--- WpfApplication1/WpfApplication1/Person.cs
+++ WpfApplication1/WpfApplication1/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WpfApplication1
 {
-    class Person
+    public class Person
     {
         public string Username { get; set; }
         public string Password { get; set; }
