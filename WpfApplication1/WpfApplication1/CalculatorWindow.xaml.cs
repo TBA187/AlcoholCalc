4cb9d2cca5d449f1e57c6ba32c716caf6ece9871
andreasbundgaard <andreasb94@live.dk>
2015-08-31 12:08:20 +0200

    Calc Window

---
+++ WpfApplication1/WpfApplication1/CalculatorWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for CalculatorWindow.xaml
+    /// </summary>
+    public partial class CalculatorWindow : Window
+    {
+        public CalculatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void UnitText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CalcButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PromilleText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AllowedText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
