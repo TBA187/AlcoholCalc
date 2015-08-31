0d7311852bad18ff9a95582b519132334a72f967
jensskjold <jmskjold@gmail.com>
2015-08-31 10:48:37 +0200

    Tests til login

--- WpfApplication1/CalcTest/UnitTest1.cs
+++ WpfApplication1/CalcTest/UnitTest1.cs
@@ -10,5 +10,26 @@ namespace CalcTest
         public void TestMethod1()
         {
         }
+
+        [TestMethod]
+        public void Login_RightInfo()
+        {
+            Login login = new Login();
+            Assert.AreEqual(true, login.Verify("Ida123", "123ida"));
+        }
+        [TestMethod]
+        public void Login_WrongInfo()
+        {
+            Login login = new Login();
+            Assert.AreEqual(false, login.Verify("Idw2322a123", "123ida"));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Login_NoInfo()
+        {
+            Login login = new Login();
+            Assert.IsNull(login.Verify(null, null));
+        }
+
     }
 }
