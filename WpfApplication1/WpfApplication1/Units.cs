7dbaa52a0a441dff980a244d2f8a7f1bae5353bf
andreasbundgaard <andreasb94@live.dk>
2015-08-31 12:21:55 +0200

    unit class

---
+++ WpfApplication1/WpfApplication1/Units.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Units
+    {
+        public string unit { get; set; }
+        public string time { get; set; }
+        public string 
+    }
+}
